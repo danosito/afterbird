deeb7ab3a21e6d8e1a762ccbe7de5c0f5c44ffb1
Dan <melekdan@yandex.ru>
2026-04-13 18:06:07 +0300

    build: extend polymer shim types for android ts build

--- ui/webui/resources/cr_elements/polymer_bundled_compat.d.ts
+++ ui/webui/resources/cr_elements/polymer_bundled_compat.d.ts
@@ -1,19 +1,41 @@
 export function html(...args: any[]): any;
 export const templatize: any;
+
+export type SpliceRecord = {
+  index: number;
+  removed: any[];
+  addedCount: number;
+};
+
+export function calculateSplices(
+  current: any[],
+  previous: any[],
+): SpliceRecord[];
+
 export function dedupingMixin<T>(mixin: T): T;
+
 export function afterNextRender(
   context: any,
   callback: (...args: any[]) => void,
   args?: any[],
 ): void;
+
 export function beforeNextRender(
   context: any,
   callback: (...args: any[]) => void,
   args?: any[],
 ): void;
-export function calculateSplices(current: any, previous: any): any;
 
-export class PolymerElement extends HTMLElement {}
+export class PolymerElement extends HTMLElement {
+  root: Document | ShadowRoot | Element;
+  connectedCallback(): void;
+  disconnectedCallback(): void;
+  ready(): void;
+  get(path: string, root?: any): any;
+  set(path: string, value: any): void;
+  notifySplices(path: string, splices: SpliceRecord[]): void;
+}
+
 export type TemplateInstanceBase = any;
 export const Polymer: any;
 export const microTask: any;
