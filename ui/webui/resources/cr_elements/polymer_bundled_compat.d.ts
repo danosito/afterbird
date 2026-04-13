db45bc30fb5b6bdc68778a056a1fef7bc915e172
Dan <melekdan@yandex.ru>
2026-04-13 17:56:48 +0300

    build: map polymer bundle imports for android cr_elements

--- ui/webui/resources/cr_elements/polymer_bundled_compat.d.ts
+++ ui/webui/resources/cr_elements/polymer_bundled_compat.d.ts
@@ -1,21 +1,25 @@
-declare module '*polymer/polymer_bundled.min.js' {
-  export function html(...args: any[]): any;
-  export const templatize: any;
-  export function dedupingMixin<T>(mixin: T): T;
-  export function afterNextRender(context: any, callback: (...args: any[]) => void, args?: any[]): void;
-  export class PolymerElement extends HTMLElement {}
-  export type TemplateInstanceBase = any;
-}
+export function html(...args: any[]): any;
+export const templatize: any;
+export function dedupingMixin<T>(mixin: T): T;
+export function afterNextRender(
+  context: any,
+  callback: (...args: any[]) => void,
+  args?: any[],
+): void;
+export function beforeNextRender(
+  context: any,
+  callback: (...args: any[]) => void,
+  args?: any[],
+): void;
+export function calculateSplices(current: any, previous: any): any;
 
-declare module 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js' {
-  export * from '*polymer/polymer_bundled.min.js';
-}
-
+export class PolymerElement extends HTMLElement {}
+export type TemplateInstanceBase = any;
+export const Polymer: any;
+export const microTask: any;
 
 declare global {
   interface HTMLElement {
     register(id: string): void;
   }
 }
-
-export {};
